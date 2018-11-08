ce784733afb936d5590a4f8c1039b6ba557d5f43
Jenny Croft <jcroft1@student.unimelb.edu.au>
2018-11-08 22:05:31 +0000

    working for binary, tf, tfidf in 1 to 1.60 seconds

--- my_retriever.py
+++ my_retriever.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-from math import *
+import math
+import time    #DEBUG ONLY
 
 class Retrieve:
     # Create new Retrieve object storing index and termWeighting scheme
@@ -8,188 +9,170 @@ class Retrieve:
         self.termWeighting = termWeighting
         
         
-        # Number of Documents in Collection
+        # Number of Documents in Collection - Will be determined as a running total
         self.collection_size = 0
         
-        # Dictionary of IDF values 
+        # Dictionary of IDF values (key = term, value = idfValue)
         self.collection_idf_values = defaultdict(float)
         
-        # Recording number f elements
+        # Recording number of elements per document (key = docid, value = number of terms in document)
         self.docid_num_elements = defaultdict(int)
         
-        # Recording list of idf + tf values per docid
-        self.docid_idf_values = defaultdict(list)
-        self.docid_tf_values= defaultdict(list)
+        # Recording list of idf + tf values per docid (key = docid, value = list of values)
+        self.docid_vector_elements = defaultdict(list)
         
+        # Recording Document Vector Magnitude for each docid ([docid] = magnitude)
+        self.docid_magnitude = defaultdict(float)
         
-        #self.document_summed_vectors = defaultdict(int)
         
 # ----------------------------------------------------------------------------        
         # Obtaining Collection Size
         set_of_docids = set()
         # Creating docid_num_elements
+        
+        #DEBUG
+        startTime = time.clock()
+        
         for term in self.index:
             for docid in self.index[term]:
                 set_of_docids.add(docid)
         self.collection_size = len(set_of_docids)
         
-# ----------------------------------------------------------------------------
-        # Calculate IDF per term        
-        if self.termWeighting == 'tfidf':
-            for term in self.index:
-                # Calculate dfw
-                num_occurrences = len(self.index[term])
-                #print("** num_ocurrences:", num_occurrences)
-                
-                idf = log10(self.collection_size/num_occurrences)
-                
-                self.collection_idf_values[term] = idf
-                    
+        #DEBUG
+        print("it took",str(time.clock()-startTime),"to loop through the entire index object and determine how many documents are in the collection")
+        
 # ----------------------------------------------------------------------------                    
-        # Calculating tf & idf per doc
-        for term in self.index:    
-            # RECORDING TERM FREQUENCY VALUES
-            for docid in self.index[term]:
-                self.docid_tf_values[docid] += self.index[term][docid]
-                self.docid_idf_values[docid] += self.index[term][docid] * self.docid_idf_values
-                self.docid_num_elements += self.index[term][docid]
-                
-                
-                
-                # NORMALISED: storing frequencies without normalisation
-                if self.termWeighting == 'binary':
-                    self.docid_num_elements[docid] += 1
-                    #self.docid_num_elements[docid] += self.index[term][docid]
-                
-                # NOT NORMALISED: Document Sizes with Term Frequency Consideration
-                else:
-                    self.document_summed_vectors[docid] += self.index[term][docid]
-# -----------------------------------------------------------------------------
-        """# Calculate Collection Size
-        for docid in self.docid_num_elements:
-            self.collection_size += 1
-        print("*** Collection size = " + str(self.collection_size)) 
-"""
-# -----------------------------------------------------------------------------        
-       
-                
-        #print("idf values:", self.idf_values)
-
-# -----------------------------------------------------------------------------        
-        # Creating document_vector_sizes for tf & tfidf
-        if self.termWeighting != 'binary':
-            
-            # Iterates through index to obtain document vector sizes relative to term weighting
+        # Loop through every term in the index and calculating tf & idf per doc and term.
+        
+        #DEBUG
+        startTime = time.clock()
+        
+        if self.termWeighting == 'tf':
+            # find the TF values for every document's terms, and the total number of elements in each document
+            # by looping over all of the terms in the entire index
             for term in self.index:
-
                 for docid in self.index[term]:
-                    # Obtains Term Frequency Value 
-                    doc_tf = self.index[term][docid]/self.docid_num_elements[docid]
-                    
-                    if self.termWeighting == 'tfidf':
-                        doc_tfidf = doc_tf * self.idf_values[term]
-                        self.document_vector_sizes[docid] += doc_tfidf
-                        
-                    else:
-                        self.document_vector_sizes[docid] += doc_tf
-                   
-                    
+                    self.docid_num_elements[docid] += self.index[term][docid]
+                    self.docid_vector_elements[docid].append(self.index[term][docid])
+            
+            # calculate the magnitude of the document vectors based on normalised TF elements.
+            for (docid, tf_values) in self.docid_vector_elements.items():
+                total = 0
+                numElems = self.docid_num_elements[docid]
+                for elem in tf_values:
+                    elem = elem/numElems
+                    total += elem * elem
+                self.docid_magnitude[docid] = math.sqrt(total)
+        
+        elif self.termWeighting == 'tfidf':
+            # find the TF and IDF values for every document's terms, and the total number of elements in each document
+            # by looping over all of the terms in the entire index
+            for term in self.index:
+                # Calculate the IDF value for this term
+                # Calculate dfw - the number of documents the term appears in
+                num_occurrences = len(self.index[term])
+                idf = math.log10(self.collection_size/num_occurrences)
+                self.collection_idf_values[term] = idf
                 
+                for docid in self.index[term]:
+                    self.docid_num_elements[docid] += self.index[term][docid]
+                    self.docid_vector_elements[docid].append(self.index[term][docid] * idf)
+            
+            # calculate the magnitude of the document vectors based on normalised TF elements * idf value.
+            for (docid, vector_elems) in self.docid_vector_elements.items():
+                total = 0
+                numElems = self.docid_num_elements[docid]
+                for elem in vector_elems:
+                    elem = elem/numElems
+                    total += elem * elem
+                self.docid_magnitude[docid] = math.sqrt(total)
         
-        
-        
+        else:
+            # LOGIC FOR BINARY WEIGHTING
+            # find the Binary values for every document's terms, and the total number of elements in each document
+            # by looping over all of the terms in the entire index
+            x = 0
+            for term in self.index:
+                for docid in self.index[term]:
+                    self.docid_num_elements[docid] += 1
                 
-
+                #print("*** number of docid elements =", self.docid_num_elements[docid], "for", docid)
+           
+            
+                self.docid_magnitude[docid] = math.sqrt(self.docid_num_elements[docid])
+                print("run:", x, ", docid:", docid, "has a docid_magnitude of", self.docid_magnitude[docid] )
+                x +=1
+            
+        
+        
+        #DEBUG
+        print("it took",str(time.clock()-startTime),"to loop through the entire index object and calculate TFs and IDFs")
+        
+          
+# -----------------------------------------------------------------------------                    
     # Method performing retrieval for specified query
     def forQuery(self, query):
         
         # keys are docids, indicating what words are inside it
-        similarity_values = defaultdict(list)
+        # (key = docid, value = cosine similarity score for this query)
+        cosine_sim_values = defaultdict(float)
         
-        # first run candidate values
-        first_run_candidates = set()
+        len_query = len(query)
         
-        # map docids to overall similarity values
-        candidate_values = defaultdict(float)
+        # first run candidate values. This is used to only check documents which have at least one
+        # similar term with the query (as all others will have a similarity of zero!)
+        # NOTE: WE SHOULD JUST SKIP THINGS IN REAL TIME INSTEAD SINCE WE ARE FORCED TO LOOP AN ENTIRE PASS ANYWAY!
+        #candidates = defaultdict(int)
         
-        #
-        qd_val = 0
+        #DEBUG
+        #startTime = time.clock()
         
-        # Obtaining candidates that contain at least one query term
-        for term in query:
-            # tests validity
-            if term in self.index.keys():
-                for docid in self.index[term]:
-                    first_run_candidates.add(docid)
-        
-        #number_of_candidates = len(first_run_candidates)
+        #DEBUG
+        #print("it took",str(time.clock()-startTime),"to build the candidate document set for query:",query)
             
-        # iterating first run candidates and calculating similarity scores
-        
-        for docid in first_run_candidates:
-            qd_val = 0
-            #print("+++ docid test:", i)
+        # looping through each document, and skipping if there are no query terms in doc, 
+        # else calculate cosine similarity and add to candidates list [docid] = cosine_sim
+        for docid in range(1, self.collection_size + 1):
+            
+            # numerator of cosine similarity equation
+            qd_dot_product = 0
+            
+            query_i = 0
+            doc_i = 0
+            
+            # sum the squares of each element as we go to eventually figure out the vector magnitude, which will be the denominator
+            if self.docid_magnitude[docid] == 0:
+                continue
+            d_vector_magnitude = self.docid_magnitude[docid]
+            #print("d_vector magnitude for", docid, "is", self.docid_magnitude[docid])
+            
             for term in query:
-                if term in self.index:
-                    for check_doc in self.index[term]:
-                        if check_doc == docid:
-                            #print("### print check_doc val:", check_doc)
-                            if self.termWeighting == 'binary':
-                                # CODE value of qd for binary
-                                qd_val += 1
-                                
-                            else: 
-                                # CODE value of qd for tf
-                                doc_i = self.index[term][docid]/self.docid_num_elements[docid]
-                                query_i = query[term]/len(query)
-                                
-                                tf_val = doc_i * query_i
-                                
-    
-                                if self.termWeighting == "tfidf":
-                                    # CODE value of qd for tfidf
-                                    qd_val += tf_val * self.idf_values[term]
-                                    
-                                # if term weighting = "tf"    
-                                else:
-                                    qd_val += tf_val
-                                
-                    # COSINE SIMILARITY
-            cosine_sim = qd_val / self.document_vector_sizes[docid]
-           # print("docid:", docid)
-           # print("**cosine sim:", cosine_sim)
-                            
-                
+                if term in self.index.keys() and docid in self.index[term].keys():
+                    if self.termWeighting == 'tf':
+                        norm_scale = 1 / self.docid_num_elements[docid]
+                        
+                        query_i = query[term]/len_query
+                        doc_i = self.index[term][docid] * norm_scale
+                        
+                        
+                    elif self.termWeighting == 'tfidf':
+                        norm_scale = 1 / self.docid_num_elements[docid]
+                        
+                        query_i = query[term]/len_query * self.collection_idf_values[term]
+                        doc_i = self.index[term][docid] * norm_scale * self.collection_idf_values[term]
+                        
+
+                    else:
+                        query_i += 1
+                        doc_i += 1
+                        
+                    qd_dot_product += query_i * doc_i
             
-                
-            
-            #print("set of first run candidates:", first_run_candidates)            
-            #print("length of first run candidates:", len(first_run_candidates))
-            
-                
+            # COSINE SIMILARITY
+            cosine_sim = qd_dot_product / d_vector_magnitude
+            cosine_sim_values[docid] = cosine_sim
         
-                  
-        
-            
-        
-        
-        
-        # if self.termWeighting == function name, then run function
-        
-            # binary - whether or not term is present - consideration of
-            # multiple occurences 
-            
-            # tf (term frequency) - frequency of term in document - considerati
-            # on of how frequent in collection
-            
-            # tfidf
-            
-                # obtain frequent words in document (number of term uses/total number of words in doc)
-                
-                # obtain inverse - log (size of collection/number of docs containing term)
-                
-                # tf.idf 
-        
-        
-        return range(1,11)
-    
+        # Finished processing all the documents and obtaining scores
+        top_10 = sorted(cosine_sim_values, key = cosine_sim_values.get, reverse = True)[:10]
+        print("***top-10:", top_10)
+        return top_10
